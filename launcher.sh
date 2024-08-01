0f6072cce48b30d32f7b88a8f58446dfc895b701
ewhitestorm <storm.eugene@yandex.ru>
2024-08-01 15:59:36 +0500

    File executable in Bash

--- launcher.sh
+++ launcher.sh
@@ -2,7 +2,7 @@
 source .venv/bin/activate
 cd app/
 uvicorn main:app --port 8080 --reload 2> ./logs/launcher.log
-result=`tac ./logs/launcher.log | grep -m 1 .`
+result=$(tac ./logs/launcher.log | head -n 1)
 if [[ "$result" == "ERROR"* ]]
 then
 sudo lsof -t -i tcp:8080 | xargs kill -9
@@ -10,4 +10,7 @@ uvicorn main:app --port 8080 --reload 2> ./logs/launcher.log
 else
 uvicorn main:app --port 8000 --reload 2> ./logs/launcher.log
 fi
+deactivate
 cd ..
+chmod +x launcher.sh
+trap './launcher.sh' EXIT
